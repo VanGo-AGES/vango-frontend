ff0b86fd012bd29945f27798e24728296d9bbb5c
Carlos Moraes <carlos.moraes003@edu.pucrs.br>
2026-03-16 18:22:29 -0300

    feat: adiciona design tokens de cores e tipografia do figma

---
+++ styles/colors.ts
@@ -0,0 +1,11 @@
+export const colors = {
+  primary: '#FFEE8C', // Yellow - CTAs / Focused states
+  secondary: '#8c9dff', // Iris - Links
+  tertiary: '#dba3f0', // Peach - Accenting Illustrations
+  dark: '#0E0E2C', // Onyx - Overlays / Shadows / Headings
+  success: '#abf2db', // Evergreen - Valid fields / Success messages
+  text: '#4A4A68', // Slate - Body text
+  subtleText: '#8C8CA1', // Light Slate - Helper / Deemphasized text
+  accent: '#ECF1F4', // Dorian - Accent color / Hairlines
+  light: '#FAFCFE', // Cloud - Light mode backgrounds / Dialogs
+};
---
+++ styles/typography.ts
@@ -0,0 +1,52 @@
+export const typography = {
+  header1: {
+    fontFamily: 'WorkSans_700Bold',
+    fontSize: 64,
+    letterSpacing: -1.28, // -2% de 64px
+  },
+  header2: {
+    fontFamily: 'WorkSans_700Bold',
+    fontSize: 40,
+    letterSpacing: -0.8, // -2% de 40px
+  },
+  header3: {
+    fontFamily: 'WorkSans_700Bold',
+    fontSize: 24,
+    letterSpacing: -0.48, // -2% de 24px
+  },
+  subtitle: {
+    fontFamily: 'WorkSans_500Medium',
+    fontSize: 24,
+  },
+  body: {
+    fontFamily: 'WorkSans_500Medium',
+    fontSize: 16,
+    lineHeight: 22.4, // 140% de 16px
+  },
+  bodyBold: {
+    fontFamily: 'WorkSans_700Bold',
+    fontSize: 16,
+    lineHeight: 22.4,
+  },
+  small: {
+    fontFamily: 'WorkSans_500Medium',
+    fontSize: 14,
+  },
+  preTitle: {
+    fontFamily: 'WorkSans_700Bold',
+    fontSize: 10,
+    letterSpacing: 0.3, // 3% de 10px
+    textTransform: 'uppercase' as const, // Adicionado pois visualmente no Figma está em maiúsculo
+  },
+  buttonText: {
+    fontFamily: 'WorkSans_700Bold',
+    fontSize: 10,
+    letterSpacing: 0.3, // 3% de 10px
+    textTransform: 'uppercase' as const,
+  },
+  link: {
+    fontFamily: 'WorkSans_700Bold',
+    fontSize: 16,
+    textDecorationLine: 'underline' as const,
+  },
+};
