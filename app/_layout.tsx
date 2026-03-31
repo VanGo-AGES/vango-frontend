01fbb4588e0953208a1131e6c84b4de9df84f526
sofia sartori <sofia.sartori@poatek.com>
2026-03-30 21:50:25 -0300

    fix: carrega fontes worksans

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,28 @@
+import { WorkSans_500Medium, WorkSans_700Bold, useFonts } from '@expo-google-fonts/work-sans';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { Stack } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
+import { useEffect } from 'react';
 import { PaperProvider } from 'react-native-paper';
 
 import { queryClient } from '@/lib/query-client';
+
+SplashScreen.preventAutoHideAsync();
 
 export const unstable_settings = {
   anchor: 'exemplo',
 };
 
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({ WorkSans_500Medium, WorkSans_700Bold });
+
+  useEffect(() => {
+    if (fontsLoaded) SplashScreen.hideAsync();
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) return null;
+
   return (
     <QueryClientProvider client={queryClient}>
       <PaperProvider>
