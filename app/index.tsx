d7fd26d333f23549030f84766e084f5c889e4030
Eduardo Paz <edu.epaz@gmail.com>
2026-05-18 18:34:15 -0300

    feat: implement driver active route details screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,10 @@
-import { Redirect } from 'expo-router';
+// import { Redirect } from 'expo-router';
+
+// export default function Index() {
+//   return <Redirect href={'/splash' as never} />;
+// }
+import DriverActiveRouteDetailsScreen from './(driver)/(route)/active-route-details-screen';
 
 export default function Index() {
-  return <Redirect href={'/splash' as never} />;
+  return <DriverActiveRouteDetailsScreen />;
 }
